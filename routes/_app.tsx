2315dbc8675450dbbb36e148618e0b5d37cd8a14
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-11-08 03:51:15 -0600

    feat: Removido codigo por defecto

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,8 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>bibliomas-fresh</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <title>Bibliomas</title>
       </head>
       <body>
         <Component />
