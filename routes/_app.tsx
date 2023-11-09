573b24acf67472e562f16bc430745fe9c282d2ed
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-11-09 15:34:59 -0600

    fix(is-cu5): Corregido viewport

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,13 +5,12 @@ export default function App({ Component }: AppProps) {
     <html>
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="manifest"
           href="/site.webmanifest"
           crossorigin="use-credentials"
         />
-        <meta name="viewport" content="viewport-fit=cover" />
         <meta name="theme-color" content="#D1C1D7" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes"/>
