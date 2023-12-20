9ec46a102d9b426329e38da11a882d16e6010754
kathyZpe <kathyzeape@gmail.com>
2023-12-19 23:46:53 -0600

    Creacion de pie de pagina

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,4 +1,6 @@
 import { RouteConfig } from "$fresh/server.ts";
+import Startnav from "@/components/Starnav.tsx";
+import Footer from "@/components/Footer.tsx";
 
 export const config: RouteConfig = {
   skipInheritedLayouts: true, // Skip already inherited layouts
@@ -7,48 +9,55 @@ export const config: RouteConfig = {
 export default function Home() {
   return (
     <>
-      <div class="navbar bg-slate-300">
-        <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </button>
-        </div>
-        <div class="flex-1">
-          <a class="btn btn-ghost font-sans">BIBLIOMAS</a>
-        </div>
-        <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-          </button>
-        </div>
-      </div>
-
+      <Startnav />
       <div class="hero bg-slate-100 ">
-      <h1 class="mt-10 font-sans text-5xl font-bold ">Bienvenido a BIBLIOMAS</h1>
+        <h1 class="mt-10 font-sans text-5xl font-bold ">Acerca de nosotros</h1>
       </div>
       <div class="hero min-h-screen bg-slate-100 ">
         <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src="/img/persona.jpg" class="max-w-lg rounded-lg shadow-2xl" />
           <div>
-            {/*<h1 class="font-sans text-5xl font-bold">Bienvenido a BIBLIOMAS</h1>*/}
-            <p class="font-sans py-6">Bienvenido a nuestro generador de referencias bibliográficas, una herramienta indispensable para simplificar el proceso de citar fuentes y construir bibliografías de manera precisa. Nos enorgullece ofrecerte una solución eficiente y fácil de usar para organizar y presentar correctamente la información de tus fuentes académicas.</p>
-            <p class="font-sans py-6">Ya sea que estés redactando un ensayo, artículo científico o cualquier otro trabajo académico, sabemos que citar correctamente es esencial para respaldar tu investigación y dar crédito a las ideas de otros. Nuestra plataforma está diseñada para facilitar este proceso, permitiéndote concentrarte en el contenido de tu trabajo sin preocuparte por los detalles técnicos de las citas.</p>
-            <p class="font-sans py-6">Explora nuestras funciones intuitivas que te guiarán a través de los diferentes formatos de citas, desde libros y artículos hasta recursos en línea. Ya seas estudiante, investigador o profesional, nuestro generador de referencias está aquí para simplificar tu experiencia y garantizar la integridad académica de tus trabajos.</p>
-            <p class="font-sans text-lg">¡Comienza ahora y experimenta la comodidad de crear referencias bibliográficas precisas con tan solo unos clics!</p>
+            <p class="font-sans py-6">¡Bienvenido a Sycorax Corp!</p>
+            <p class="font-sans py-6">
+              En el vertiginoso mundo académico y profesional, la gestión
+              eficiente de referencias bibliográficas es esencial para el éxito
+              y la credibilidad. En Sycorax Corp, entendemos la importancia de
+              simplificar este proceso crucial, permitiéndote centrarte en lo
+              que realmente importa: tu investigación.
+            </p>
+            <p class="font-sans py-6">
+              En Sycorax Corp, nos enorgullece ofrecer una herramienta que va
+              más allá de la simple organización de citas. Con características
+              innovadoras y una interfaz amigable, nuestro gestor de referencias
+              bibliográficas te ayuda a mantener la coherencia, evita errores y
+              facilita la colaboración en equipo.
+            </p>
+            <p class="font-sans py-6">
+              Explora el futuro de la gestión de referencias bibliográficas con
+              Sycorax Corp. Únete a nosotros mientras transformamos la manera en
+              que accedes, organizas y compartes tus fuentes, permitiéndote
+              destacar en tu investigación y alcanzar nuevos niveles de
+              excelencia.
+            </p>
+            <p class="font-sans py-6">
+              Descubre la diferencia con Sycorax Corp. Simplifica tu trabajo.
+              Eleva tu investigación.
+            </p>
           </div>
+          <img src="/img/computer.jpg" class="max-w-lg rounded-lg shadow-2xl" />
         </div>
       </div>
-
-      {/*<Button state="btn-primary" type="button">
+      <Footer />
+      {
+        /*<Button state="btn-primary" type="button">
         <span>Clickeame</span>
       </Button>
       <div class="bg-[#50d71e]">
-        <nav class="navbar font-mono">hola buenos dias</nav>  botttones y taps 
+        <nav class="navbar font-mono">hola buenos dias</nav>  botttones y taps
         <h1 class="font-sans">Hola mundo</h1>
         <IconGenderMale size={48} />
         <img src="/img/computer.jpg" />
-      </div>*/}
-
+      </div>*/
+      }
     </>
   );
 }
--- routes/suscription.tsx
+++ routes/suscription.tsx
@@ -1,5 +1,6 @@
 import { RouteConfig } from "$fresh/server.ts";
-import Startnav from "@/components/Starnav.tsx"
+import Startnav from "@/components/Starnav.tsx";
+import Footer from "@/components/Footer.tsx";
 
 export const config: RouteConfig = {
   skipInheritedLayouts: true, // Skip already inherited layouts
@@ -10,20 +11,50 @@ export default function Home() {
     <>
       <Startnav />
       <div class="hero bg-slate-100 ">
-      <h1 class="mt-10 font-sans text-5xl font-bold ">Bienvenido a BIBLIOMAS</h1>
+        <h1 class="mt-5 font-sans text-5xl font-bold ">
+          Suscripciones a Bibliomas
+        </h1>
       </div>
+
       <div class="hero min-h-screen bg-slate-100 ">
         <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src="/img/persona.jpg" class="max-w-lg rounded-lg shadow-2xl" />
-          <div>
-            {/*<h1 class="font-sans text-5xl font-bold">Bienvenido a BIBLIOMAS</h1>*/}
-            <p class="font-sans py-6">Bienvenido a nuestro generador de referencias bibliográficas, una herramienta indispensable para simplificar el proceso de citar fuentes y construir bibliografías de manera precisa. Nos enorgullece ofrecerte una solución eficiente y fácil de usar para organizar y presentar correctamente la información de tus fuentes académicas.</p>
-            <p class="font-sans py-6">Ya sea que estés redactando un ensayo, artículo científico o cualquier otro trabajo académico, sabemos que citar correctamente es esencial para respaldar tu investigación y dar crédito a las ideas de otros. Nuestra plataforma está diseñada para facilitar este proceso, permitiéndote concentrarte en el contenido de tu trabajo sin preocuparte por los detalles técnicos de las citas.</p>
-            <p class="font-sans py-6">Explora nuestras funciones intuitivas que te guiarán a través de los diferentes formatos de citas, desde libros y artículos hasta recursos en línea. Ya seas estudiante, investigador o profesional, nuestro generador de referencias está aquí para simplificar tu experiencia y garantizar la integridad académica de tus trabajos.</p>
-            <p class="font-sans text-lg">¡Comienza ahora y experimenta la comodidad de crear referencias bibliográficas precisas con tan solo unos clics!</p>
+          <img src="/img/libro.jpg" class="max-w-2xl rounded-lg shadow-2xl" />
+
+          <div class="flex flex-col space-y-4 mx-5">
+            <div class=" card w-96 bg-gray-400 shadow-xl image-full">
+              <figure>
+                <img
+                  src="/img/onebook.jpg"
+                  alt="Abook"
+                />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title font-sans text-slate-200 text-3xl">Paquete estándar</h2>
+                <p class="font-sans text-slate-200 text-xl">$100.00/mes</p>
+                <div class="card-actions justify-end">
+                  <button class="btn btn-primary font-sans">Suscribete</button>
+                </div>
+              </div>
+            </div>
+            <div class=" card w-96 bg-gray-500 shadow-xl image-full">
+              <figure>
+                <img
+                  src="/img/books.jpg"
+                  alt="Books"
+                />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title font-sans text-slate-200 text-3xl">Paquete empresarial</h2>
+                <p class="font-sans text-slate-200 text-xl">$2000.00/mes</p>
+                <div class="card-actions justify-end">
+                  <button class="btn btn-primary font-sans">Suscribete</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
