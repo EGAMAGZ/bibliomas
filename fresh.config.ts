6e0589d5e77fd3b28f19182575337d9589300838
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-01-11 18:02:37 -0600

    feat: Actualizado taildwind (#28)

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,16 +1,8 @@
-import tailwindPlugin from "@tailwindcss";
 import { defineConfig } from "$fresh/server.ts";
-import tailwindConfig from "@/tailwind.config.ts";
-import storiesPlugin from "https://deno.land/x/fresh_stories@0.0.4/stories-plugin.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
   plugins: [
-    tailwindPlugin({
-      mode: "production",
-      input: "./styles.css",
-      verbose: false,
-      tailwindConfig,
-    }),
-    storiesPlugin(),
+    tailwind(),
   ],
 });
